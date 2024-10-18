66c25cfe8b94ce8b054d76b92303a0b07f6c49bb
Benjamin Davies <148213274+BenDavies1218@users.noreply.github.com>
2024-10-19 00:03:07 +1000

    feat: created folder structure
    signup, signin auth page

---
+++ crypto-order/src/app/(login)/signin/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <>
+      <h1>Hello from the Signin</h1>
+    </>
+  );
+}
--- crypto-order/src/app/page.tsx
+++ crypto-order/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 import { api, HydrateClient } from "~/trpc/server";
 
@@ -7,9 +8,14 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex h-full items-center justify-center bg-white">
+      <main className="flex h-full items-center justify-center bg-blue-500">
         <h1>Crypto Order</h1>
-        <Link href={"/order"}>Order Page</Link>
+        <Link href={"/signin"}>
+          <Button>Sign In</Button>
+        </Link>
+        <Link href={"/signup"}>
+          <Button>Create an Account</Button>
+        </Link>
       </main>
     </HydrateClient>
   );
